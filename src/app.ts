6e7c40cabaa91863d370fdf367dadbe131a18f0a
Edwin Ortiz <edarcode@gmail.com>
2024-06-22 02:51:40 -0500

    agregando middle de cors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { notFound } from "./controllers/notFound";
@@ -6,6 +7,7 @@ import { usersRouter } from "./routes/users";
 dotenv.config();
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", usersRouter);
