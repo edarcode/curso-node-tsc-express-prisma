635115d19cad9bda63c20c014109fbd50b16bf67
Edwin Ortiz <edarcode@gmail.com>
2024-06-24 14:18:55 -0500

    refactor

--- src/controllers/users/getUsers.ts
+++ src/controllers/users/getUsers.ts
@@ -1,9 +1,9 @@
-import { ReqQueryParsed, Res } from "../../../types/types";
+import { QueryParsedGetUsers, ReqQueryParsed, Res } from "../../../types/types";
 import { getUsersDb } from "../../db/user/getUsersDb";
 
 export const getUsers = async (req: ReqQueryParsed, res: Res) => {
   try {
-    const users = await getUsersDb(req.queryParsed);
+    const users = await getUsersDb(req.queryParsed as QueryParsedGetUsers);
     res.json(users);
   } catch (error) {
     console.error(error);
